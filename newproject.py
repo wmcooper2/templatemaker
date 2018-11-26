a41e9e6e38a3732b75963ab95e64f21f65f9a280
wmc2tokyo <wmcooper2@yahoo.com>
2018-11-26 18:03:30 +0900

    Finished newproject.py. Need to write tests next.

--- newproject.py
+++ newproject.py
@@ -13,48 +13,74 @@
 """
 
 #stand lib
+import os
 from pathlib import Path
 
-dirs    = ["src", "data", "testdata"]
-files   = ["NOTES.md", "README.md", ".gitignore"]
+rootdir     = input("Give the project a name: ")
+rootdir     = "./"+str(rootdir)
+subdirs     = ["src", "data", "testdata"]
+files       = ["NOTES.md", "README.md", ".gitignore", "run", "Test"]
+testlines   = ["#!/bin/bash", "python3 -m pytest *_test.py"]
+gitignores  = [".gitignore", "NOTES.md", "data/", "testdir/"]
+runlines    = ["#!/bin/bash", "python3 "+rootdir.strip("./")+".py"]
+shellscripts= ["run", "Test"]
 
-rootdir = input("Give the project a name: ")
-rootdir = "./"+str(rootdir)
+def make_rootdir(rootdir):
+    """Makes project root dir. Returns None."""
+    if not Path(rootdir).exists():
+        Path(rootdir).mkdir()
 
-def make_run():
-    """"""
-    pass
+def make_subdirs(subdirs):
+    """Makes sub directories under the root dir. Returns None."""
+    for el in subdirs:
+        el = rootdir+"/"+str(el)
+        if not Path(el).exists():
+            Path(el).mkdir()
 
-def make_test():
-    """"""
-    pass
-
-def make_files():
+def make_files(files):
     """Makes files under the root dir. Returns None."""
     for el in files:
         el = rootdir+"/"+str(el)
         if not Path(el).exists():
             Path(el).touch()
 
+def edit_test(testlines):
+    """Writes to 'Test' file. Returns None."""
+    with open(rootdir+"/Test", "w+") as f:
+        for el in testlines:
+            f.write(el)
+            f.write("\n")
 
-def make_subdirs():
-    """Makes sub directories under the root dir. Returns None."""
-    for el in dirs:
-        el = rootdir+"/"+str(el)
-        if not Path(el).exists():
-            Path(el).mkdir()
+def edit_gitignore(gitignores):
+    """Writes to '.gitignore'. Returns None."""
+    with open(rootdir+"/.gitignore", "w+") as f:
+        for el in gitignores:
+            f.write(el)
+            f.write("\n")
 
-def make_rootdir():
-    """Makes project root dir. Returns None."""
-    if not Path(rootdir).exists():
-        Path(rootdir).mkdir()
+def edit_run(runlines):
+    """Writes to 'run' file. Returns None."""
+    with open(rootdir+"/run", "w+") as f:
+        for el in runlines:
+            f.write(el)
+            f.write("\n")
 
+def chmods(shellscripts):
+    """Changes user permissions of files. Returns None."""
+    for el in shellscripts:
+        os.chmod(Path(rootdir+"/"+el), 0o755)
+
+def makemain(rootdir):
+    """Makes the main script in <root>/src/ dir. Returns None."""
+    main = rootdir.strip("./")
+    Path(rootdir+"/src/"+main+".py").touch()
+    
 if __name__ == "__main__":
-    make_rootdir()
-    make_subdirs()
-#    make_run()
-#    make_test()
-    make_files()
-
-
-
+    make_rootdir(rootdir)
+    make_subdirs(subdirs)
+    make_files(files)
+    edit_test(testlines)
+    edit_run(runlines)
+    edit_gitignore(gitignores)
+    chmods(shellscripts)
+    makemain(rootdir)
